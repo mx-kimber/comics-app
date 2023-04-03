5e0e538b1597e425cfe348b00fd436b26cad33ac
Kimber Ogden <mx.kimber@gmail.com>
2023-04-02 23:47:09 -0500

    Added create action

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -4,11 +4,24 @@ class ComicsController < ApplicationController
     @comics = Comic.all
     render :index
   end
+
   def show
     @comic = Comic.find_by(id: params[:id])
     render :show
   end
 
+  def create
+    @comic = Comic.new(
+      comic: title = params[:title]
+      comic: writer = params[:writer]
+      comic: illustrator = params[:illustrator]
+      comic: year = params[:year]
+      comic: genre = params[:genre]
+      comic: image_url = params[:image_url]
+    )
+    @comic.save
+    render :show
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "/comics" => "comics#index"
   get "/comics/:id" => "comics#show"
+  post "/comics" => "comics#create"
 
 
 end
